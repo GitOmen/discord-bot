08667133881fe9a922f3c3936b31e55ce5a5d29a
GitOmen <86669185+GitOmen@users.noreply.github.com>
2021-11-08 00:10:44 +0100

    refactor: remove obsolete commands

--- bot.py
+++ bot.py
@@ -128,33 +128,6 @@ async def clear_error(ctx, error):
 
 
 @client.command()
-@commands.has_permissions(kick_members=True)
-async def kick(ctx, member: discord.Member, *, reason=None):
-    await member.kick(reason=reason)
-
-
-@client.command()
-@commands.has_permissions(ban_members=True)
-async def ban(ctx, member: discord.Member, *, reason=None):
-    await member.ban(reason=reason)
-    await ctx.send(f'Banned {member.mention}')
-
-
-@client.command()
-async def unban(ctx, *, member):
-    banned_users = await ctx.guild.bans()
-    member_name, member_discriminator = member.split('#')
-
-    for ban_entry in banned_users:
-        user = ban_entry.user
-
-        if (user.name, user.discriminator) == (member_name, member_discriminator):
-            await ctx.guild.unban(user)
-            await ctx.send(f'Unbanned {user.mention}')
-            return
-
-
-@client.command()
 async def set_log_channel(ctx):
     with open('config.json', 'r') as f:
         config = json.load(f)
